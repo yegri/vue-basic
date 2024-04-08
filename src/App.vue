f6691fa9cb0e64e064d2cb9b8d9cab3c9b7c31a1
Yeeun Lee <exsilver305@gmail.com>
2024-04-09 00:54:43 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+  <h1>Vue.js 라이프사이클 테스트</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  beforeCreate() {
+    console.log("lifeCycle is beforeCreate", this.count);
+  },
+  created() {
+    console.log("lifeCycle is created", this.count);
+  },
+  beforeMount() {
+    console.log("lifeCycle is beforeMount", document.querySelector("h1"));
+  },
+  mounted() {
+    console.log("lifeCycle is mounted", document.querySelector("h1"));
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
