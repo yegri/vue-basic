30e48000c43afcd8d77f5c7e9d40d0cf5b2995b1
Yeeun Lee <exsilver305@gmail.com>
2024-04-10 20:27:01 +0900

    OptionsAPI props

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
 <template>
-  <button @click="changeMessage">{{ message }}</button>
+  <div>
+    <ChildComponent
+      v-bind:sendProps1="title"
+      v-bind:sendProps2="createdAt"
+      v-bind:sendProps3="obj"
+    />
+  </div>
 </template>
 
 <script>
+import ChildComponent from "./components/ChildComponent.vue";
+
 export default {
+  components: {
+    ChildComponent,
+  },
   data() {
     return {
-      message: "안녕하세요. Vue.js Watch 기능 테스트 오리지널 문구",
+      title: "부모 컴포넌트에서 선언된 데이터입니다.",
+      createdAt: 2024,
+      obj: {
+        id: 2024,
+        name: "John",
+      },
     };
-  },
-
-  // message라는 변수가 바뀌는 것을 지켜보고 있다가 changeMessage()를 통해 바뀌게 되면 alert창을 띄워줌
-  // 데이터 뿐만 아니라 computed로 계산된 형태의 데이터도 watch로 감지할 수 있음
-  // 보통 게시판에서 한 컬럼을 선택하였을 때, 고유한 id 값이 바뀜을 감지하고
-  // 이때, 그 id 값에 따른 상세 데이터를 호출할 때 주로 사용함
-  watch: {
-    message() {
-      window.alert("message 변수에 담긴 데이터가 변경되었습니다.");
-    },
-    id() {
-      // 해당 상세데이터를 조회하는 api 호출
-    },
-  },
-
-  methods: {
-    changeMessage() {
-      console.log("함수 호출");
-      this.message = "변경된 message 데이터입니다.";
-    },
   },
 };
 </script>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ sendProps1 }}</div>
+  <div>{{ sendProps2 }}</div>
+  <div>{{ sendProps3.id }}</div>
+  <div>{{ sendProps3.name }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    sendProps1: String,
+    sendProps2: Number,
+    sendProps3: Object,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
